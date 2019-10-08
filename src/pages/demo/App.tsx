b1b34b926ca82a14c7f3e2a9553990dbc34a4433
zhangliGit <674937076@qq.com>
2019-10-08 22:28:34 +0800

    代码优化

--- src/pages/demo/App.tsx
+++ src/pages/demo/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.less'
+import ChildOne from './ChildOne'
 
 const App: React.FC = () => {
+  const params = {
+    title: 'haha'
+  }
   return (
     <div className="qui-page app" style={{ background: "red" }}>
       <div className="header"></div>
+      <ChildOne {...params}></ChildOne>
     </div>
   );
 }
