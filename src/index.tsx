03f7ae8e30fb9b6b241cd6e509b56b871e2dcb2d
zhangli <674947076@qq.com>
2019-10-08 18:00:06 +0800

    代码优化

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import '@a/css/qui-base.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
