2a8652330048a93c262f0550ed7bfb3c86e4d103
zhangliGit <674937076@qq.com>
2019-10-15 22:47:49 +0800

    代码优化

--- src/pages/demo/Detail.tsx
+++ src/pages/demo/Detail.tsx
@@ -1,26 +1,34 @@
-import React, {useState, useEffect } from 'react';
+import React from 'react';
 import $ajax from "../../utils/ajax-serve";
 import ScrollList from "../../components/common/ScrollList/index";
 
-const Detail: React.FC = () => {
-  const [data, setData] = useState('')
-  useEffect(() => {
+class Detail extends React.Component {
+  readonly state = {
+    data: 'zhangli'
+  }
+  componentDidMount() {
     $ajax
       .get({
         url: "http://yapi.demo.qunar.com/mock/5691/detail"
       })
       .then(res => {
-        setData(res.data);
+        this.setState({
+          data: res.data
+        })
       });
-  }, [])
-  return (
-    <div className="qui-page qui-fx-ver app">
-      <div className="header">详情</div>
-      <ScrollList>
-        <div style={{padding: '15px', lineHeight: '35px'}}>{data}</div>
-      </ScrollList>
-    </div>
-  );
+  }
+  render () {
+    console.log(this.props)
+    return (
+      <div className="qui-page qui-fx-ver app">
+        <div className="header">详情</div>
+        <ScrollList>
+          <div style={{padding: '15px', lineHeight: '35px'}}>{this.state.data}</div>
+        </ScrollList>
+      </div>
+    )
+  }
 }
 
-export default Detail;
+
+export default Detail
